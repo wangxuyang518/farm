c18b33d98bda0aebef688a2f0b61a219eedd412c
wxy <1044393721@qq.com>
2018-06-15 00:02:45 +0800

    Signed-off-by: wxy <1044393721@qq.com>

---
+++ app/src/main/java/project/farm/MainActivity.kt
@@ -0,0 +1,72 @@
+package project.farm
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+import android.support.design.internal.BottomNavigationItemView
+
+import android.support.design.internal.BottomNavigationMenuView
+import android.util.Log
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_publish -> {
+                message.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_order -> {
+                message.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_me -> {
+                message.setText(R.string.title_me)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        BottomNavigationViewHelper.disableShiftMode(navigation);
+    }
+
+    object BottomNavigationViewHelper {
+        @SuppressLint("RestrictedApi")
+        fun disableShiftMode(view: BottomNavigationView) {
+            val menuView = view.getChildAt(0) as BottomNavigationMenuView
+            try {
+                val shiftingMode = menuView.javaClass.getDeclaredField("mShiftingMode")
+                shiftingMode.isAccessible = true
+                shiftingMode.setBoolean(menuView, false)
+                shiftingMode.isAccessible = false
+                for (i in 0 until menuView.childCount) {
+                    val item = menuView.getChildAt(i) as BottomNavigationItemView
+
+                    item.setShiftingMode(false)
+                    // set once again checked value, so view will be updated
+
+                    item.setChecked(item.itemData.isChecked)
+                }
+            } catch (e: NoSuchFieldException) {
+                Log.e("BNVHelper", "Unable to get shift mode field", e)
+            } catch (e: IllegalAccessException) {
+                Log.e("BNVHelper", "Unable to change value of shift mode", e)
+            }
+
+        }
+    }
+}
